378ae9b9ed3732ae9756d7b8d6ebf3c404bbdc40
jashimgit <jashim.ncn@gmail.com>
2021-04-20 04:55:54 +0600

    Removed firebase config

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -4,12 +4,12 @@ import "firebase/auth";
 import { createContext, useContext, useEffect, useState } from "react";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDeYpFB2gHEVeIPe-lREviQCQbgLFB3g1g",
-  authDomain: "fir-auth-72fca.firebaseapp.com",
-  projectId: "fir-auth-72fca",
-  storageBucket: "fir-auth-72fca.appspot.com",
-  messagingSenderId: "824629658746",
-  appId: "1:824629658746:web:667bb25c38bef6f05d67c3",
+  apiKey:process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId:process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId:process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
